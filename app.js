cfda0075722951c4c5d600777776f08a7a212205
s22017 <s22017@std.it-college.ac.jp>
2024-02-15 14:16:50 +0900

    register made

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require("express-session");
+const passport = require("passport");
+const {config} = require("./utils/auth")
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(session({
+    secret: "</2aiG^bd29iC5rj)=G?mKTmnjoj",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 }
+}));
+app.use(passport.authenticate("session"));
+app.use(config(passport))
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {calcHash, generateSalt} = require("../utils/auth");
+const { PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
+
+router.post("/register", async (req, res, next) => {
+    const {email, pass} = req.body;
+    const name = req.body?.name
+    try {
+        const salt = generateSalt();
+        const hashedPassword = calcHash(pass, salt);
+        await prisma.user.create({
+            data: {
+                email,
+                name,
+                password: hashedPassword,
+                salt
+            },
+        });
+        return res.status(201).json({ result: 'created' });
+    } catch (error) {
+        if (error.code === 'P2002') {
+            return res.status(409).json({ result: 'NG', message: 'メールアドレス重複エラー' });
+        }
+        return res.status(500).json({ result: 'NG' });
+    }
+});
+
 
 module.exports = router;
